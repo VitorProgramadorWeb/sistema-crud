8524a3b27c2da32de8710e828c69918c2981d6c8
VitorProgramadorWeb <149249138+VitorProgramadorWeb@users.noreply.github.com>
2023-11-22 11:51:28 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <title>Sistema CRUD</title>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="estilo.css" type="text/css">
+</head>
+<body>
+    <nav>
+        <ul class="menu">
+            <li class="item-menu">Inserir</li>
+            <li class="item-menu">Listar</li>
+            <li class="item-menu">Alterar</li>
+            <li class="item-menu">Deletar</li>
+        </ul>
+    </nav>
+
+    <div>
+conteudo
+    </div>
+
+    <footer>
+        <span>Desenvolvido por: Vítor</span>
+    </footer>
+</body>
+</html>
